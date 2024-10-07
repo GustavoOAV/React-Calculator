03e057d656dec668bad2f6d6d64075433779f312
Gustavo Orlando <gudmx100@hotmail.com>
2024-10-07 17:00:46 -0300

    Calculadora em React

---
+++ src/components/button/styles.js
@@ -0,0 +1,20 @@
+import styled from 'styled-components';
+
+export const ButtonContainer = styled.div`
+    padding: 20px;
+    border: 1px solid #ffffff;
+    background-color: black;
+    font-size: 24px;
+    font-weight: 700;
+    flex: 1;
+    color: white;
+    
+    
+
+    &:hover {
+    opacity: 0.8;
+    }
+   
+
+
+`
---
+++ src/components/input/styles.js
@@ -0,0 +1,29 @@
+import styled from "styled-components";
+
+export const InputContainer = styled.div`
+  width: 100%;
+  height: 85px;
+  background-color: #000000;
+
+  display: flex;
+  align-items: center;
+  justify-content: flex-end
+ 
+  font-size: 24px;
+  font-family: "Roboto";
+  color: white;
+
+
+  input {
+    width: 100%;
+    height: 85px;
+    background-color: #000000;
+    border: 0;
+    align: right;
+    font-size: 24px;
+    font-family: "Roboto";
+    padding: 0 10px;
+    color: #ffffff;
+    
+  }
+`;
